51fb3c7202e4c192dc502484e4bda140a6ff33e9
Denisha Garner <thegarnercode@gmail.com>
2022-03-09 17:39:09 -0500

    commit tree 0dfe9d88567bd7af405962b275a3aff1b6b2da5c

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+    return string.toUpperCase();
+
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log("hello".toUpperCase())
+}
+function logWhisper(string){
+    console.log("hello".toLowerCase())
+}
+
+function sayHiToHeadphonedRoomate(){
+    return ().toUpperCase
+}
+
